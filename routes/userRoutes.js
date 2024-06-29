c2f35e792619ea77a715ee5dc33ea6cf5c032b57
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-06-30 01:48:58 +0530

    code updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,53 +1,8 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
-
+import { login, register } from '../controllers/userController.js';
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-  const { name, dob, email, password } = req.body;
-
-  if (!name || !dob || !email || !password) {
-    return res.status(400).json({ message: 'All fields are required' });
-  }
-
-  try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ name, dob, email, password: hashedPassword });
-    await newUser.save();
-
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(201).json({ token, user: newUser });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: 'User not found' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
-    }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    res.status(200).json({ token, user });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+router.post('/register',register);
+router.post('/login',login);
 
 export default router;
